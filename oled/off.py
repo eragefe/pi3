e6a17ac702b60a68003b9438b403205bced257d6
gefe <georgios@gdis.se>
2019-03-07 00:33:50 +0100

    Update off.py

--- oled/off.py
+++ oled/off.py
@@ -4,7 +4,7 @@ from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
 
-serial = i2c(port=0, address=0x3C)
+serial = i2c(port=1, address=0x3C)
 device = sh1106(serial, rotate=2)
 
 device.hide()
