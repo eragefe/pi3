b913e478ff51f1675311b3ffa95a52b720d49439
gefe <georgios@gdis.se>
2019-06-20 20:49:03 +0200

    Update input.py

--- input.py
+++ input.py
@@ -12,11 +12,20 @@ state13 = GPIO.input(13)
 if state6==1:
    GPIO.output(6, GPIO.LOW)
    GPIO.output(13, GPIO.LOW)
+   file = open("/boot/input","w")
+   file.write("coax")
+   file.close()
 
 if state6==0:
-   GPIO.output(6, GPIO.HIGH)
+   GPIO.output(6, GPIO.LOW)
    GPIO.output(13, GPIO.HIGH)
+   file = open("/boot/input","w")
+   file.write("streamer")
+   file.close()
 
 if state13:
    GPIO.output(6, GPIO.HIGH)
    GPIO.output(13, GPIO.LOW)
+   file = open("/boot/input","w")
+   file.write("opt")
+   file.close()
