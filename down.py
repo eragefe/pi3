caa0c7151a316cc67d488f831275dbf4f7d0a3dc
gefe <georgios@gdis.se>
2019-06-21 01:13:38 +0200

    Update down.py

--- down.py
+++ down.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(31,GPIO.OUT)
-GPIO.output(31,GPIO.HIGH)
+GPIO.setup(17,GPIO.OUT)
+GPIO.output(17,GPIO.HIGH)
 time.sleep(0.3)
-GPIO.output(31,GPIO.LOW)
+GPIO.output(17,GPIO.LOW)
