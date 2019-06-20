01ef2c253e689dc9b5bec8438ef17f810c5355a5
gefe <georgios@gdis.se>
2019-06-20 20:47:57 +0200

    Update default.py

--- default.py
+++ default.py
@@ -3,4 +3,23 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(13, GPIO.OUT)
-GPIO.output(13, GPIO.HIGH)
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+
+with open("/boot/input") as file:
+   contents = file.read()
+   if "streamer" in contents:
+      GPIO.output(13, GPIO.HIGH)
+      GPIO.output(6, GPIO.LOW)
+
+with open("/boot/input") as file:
+   contents = file.read()
+   if "opt" in contents:
+      GPIO.output(13, GPIO.LOW)
+      GPIO.output(6, GPIO.HIGH)
+
+with open("/boot/input") as file:
+   contents = file.read()
+   if "coax" in contents:
+      GPIO.output(6, GPIO.LOW)
+      GPIO.output(13, GPIO.LOW)
