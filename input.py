f7262982a6099af8dc2c2abf3093157bc9765703
gefe <georgios@gdis.se>
2019-08-20 22:32:46 +0200

    Update input.py

--- input.py
+++ input.py
@@ -1,4 +1,6 @@
 import RPi.GPIO as GPIO
+import os
+import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -6,26 +8,37 @@ GPIO.setup(26, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 
 # To read the state
-state6 = GPIO.input(26)
+state26 = GPIO.input(26)
 state13 = GPIO.input(13)
 
-if state6==1:
+if state26==1:
+   os.system('mpc pause')
+   GPIO.output(13, GPIO.HIGH)
+   time.sleep(0.2)
+   os.system('aplay /root/coaxial.wav')
    GPIO.output(26, GPIO.LOW)
    GPIO.output(13, GPIO.LOW)
    file = open("/boot/input","w")
-   file.write("opt")
+   file.write("coax")
    file.close()
 
-if state6==0:
+if state26==0 and state13==0:
+   os.system('mpc pause')
+   GPIO.output(13, GPIO.HIGH)
+   time.sleep(0.2)
+   os.system('aplay /root/streamer.wav')
    GPIO.output(26, GPIO.LOW)
    GPIO.output(13, GPIO.HIGH)
    file = open("/boot/input","w")
    file.write("streamer")
    file.close()
 
-if state13:
+if state13==1:
+   os.system('mpc pause')
+   time.sleep(0.2)
+   os.system('aplay /root/optical.wav')
    GPIO.output(26, GPIO.HIGH)
    GPIO.output(13, GPIO.LOW)
    file = open("/boot/input","w")
-   file.write("coax")
+   file.write("opt")
    file.close()
