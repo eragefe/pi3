0d9449547c652b4bf7520185e377887540b9af33
gefe <georgios@gdis.se>
2019-09-10 00:07:32 +0200

    Update default.py

--- default.py
+++ default.py
@@ -1,9 +1,23 @@
 import RPi.GPIO as GPIO
+import os
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(13, GPIO.OUT)
-GPIO.setup(6, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+
+eth = os.popen('ip addr show eth0 | grep "\<inet\>" | awk \'{ print $2 }\' | awk -F "/" \'{ print $1 }\'').read().strip()
+wlan = os.popen('ip addr show wlan0 | grep "\<inet\>" | awk \'{ print $2 }\' | awk -F "/" \'{ print $1 }\'').read().strip()
+
+if eth != "":
+   GPIO.output(13, GPIO.HIGH)
+   GPIO.output(26, GPIO.LOW)
+   os.system('aplay /root/ethernet.wav')
+
+if wlan != "":
+   GPIO.output(13, GPIO.HIGH)
+   GPIO.output(26, GPIO.LOW)
+   os.system('aplay /root/wifi.wav')
 
 with open("/boot/input") as file:
    contents = file.read()
